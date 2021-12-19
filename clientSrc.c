22ca305830d007a74ba1ebf6513a249820d82b29
Math <philipponmathis@gmail.com>
2021-12-19 21:36:12 +0100

    Added GTK Test window and GTK to cmake

--- clientSrc.c
+++ clientSrc.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <gtk/gtk.h>
 
-int main(){
+static void
+activate(GtkApplication *app,
+         gpointer user_data) {
+    GtkWidget *window;
 
-    return EXIT_SUCCESS;
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Salut Mon Pote !");
+    gtk_widget_show_all(window);
 }
+
+int
+main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example",
+                              G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate",
+                     G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return (status);
+}
+
