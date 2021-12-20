262b87836acf5c06040834ac2ee281daedc65d5e
Math <philipponmathis@gmail.com>
2021-12-20 15:59:57 +0100

    Parser written, TODO: conflict with GTK parser

--- clientSrc.c
+++ clientSrc.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <sqlite3.h>
+#include <string.h>
 
 //TODO: Would be interesting to add these to a specific .h
+
 void destroy (GSimpleAction *action, GVariant *parameter, gpointer user_data){
-    exit(0);
+    exit(-1);
 }
 
 void activate(GtkApplication *client, gpointer user_data) {
@@ -42,8 +44,7 @@ void activate(GtkApplication *client, gpointer user_data) {
     ///Activate Window
     gtk_window_present(GTK_WINDOW (clientWindow));
 }
-
-int main(int argc, char **argv) {
+int createWindow(int argc, char **argv){
     GtkApplication *client;
     int status;
 
@@ -57,4 +58,38 @@ int main(int argc, char **argv) {
     return status;
 }
 
+void parseArgs(char* target, int argc, char **argv){
+    char flagWrongSyntax = 0;
 
+    if (argc > 100){//If over 100 arguments, we consider the syntax to be wrong
+        flagWrongSyntax = 1;
+        strcat(target, "0");
+    }
+
+    for (int i = 1; i < argc && !flagWrongSyntax; ++i) {//We parse each argument
+        if (argv[i][0] == '-'){//If the argument starts with - (parameter)
+            switch (argv[i][1]) {//Switch depending on the parameter
+                case 'h'://help
+                    strcat(target, "h");
+                    break;
+                case 'l'://non visual, line execution
+                    strcat(target, "l");
+                    break;
+                default://Ignore incorrect parameters
+                    break;
+            }
+        }else{
+            flagWrongSyntax = 1;
+            strcat(target, "0");
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    char parsedArguments[255] = "";
+    parseArgs(parsedArguments, argc, argv);
+    fprintf(stdout, "%s", parsedArguments);
+    createWindow(argc, argv);
+
+    return 0;
+}
