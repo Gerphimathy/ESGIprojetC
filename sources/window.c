a82766a6ca3742848b1c7be60cc7c0c0f4cd4045
Math <philipponmathis@gmail.com>
2021-12-27 19:18:46 +0100

    GUI registering now works

--- sources/window.c
+++ sources/window.c
@@ -21,7 +21,6 @@ void onProfilesListScroll(GtkAdjustment *scale, gpointer data){
     windowData *uData = data;
 
     GtkLabel *error = GTK_LABEL(gtk_builder_get_object(uData->builder, "profileListError"));
-    gtk_label_set_text(error, "Error: Could not access local database");
 
     GtkLabel *profileLabels[] = {
             GTK_LABEL(gtk_builder_get_object(uData->builder, "profileName1")),
@@ -52,7 +51,42 @@ void onProfilesListScroll(GtkAdjustment *scale, gpointer data){
     }
 }
 
+/**
+ * @usage treat register button press
+ * @param registerButton -- register button widget
+ * @param data -- user data
+ */
+void onRegister(GtkButton *registerButton, gpointer data){
+    windowData *uData = data;
+    GtkLabel * error = GTK_LABEL(gtk_builder_get_object(uData->builder, "registerError"));
 
+    char username[255];
+    char password[255];
+
+    GtkEntry * userInput = GTK_ENTRY(gtk_builder_get_object(uData->builder, "registerUsername"));
+    GtkEntry * passwordInput = GTK_ENTRY(gtk_builder_get_object(uData->builder, "registerPassword"));
+
+    if(checkForSpeChars(gtk_entry_get_text(userInput)) == CHECK_OK||checkForSpeChars(gtk_entry_get_text(passwordInput))==CHECK_OK){
+        if(strlen(gtk_entry_get_text(userInput))<=255&&strlen(gtk_entry_get_text(passwordInput))<=255){
+            strcpy(username, gtk_entry_get_text(userInput));
+            strcpy(password, gtk_entry_get_text(passwordInput));
+            int regStatus = registerAccount(uData->db, username, password);
+            switch (regStatus) {
+                case REGISTER_SUCCESS:
+                    gtk_label_set_text(error, "Profile successfully created");
+                    break;
+                case REGISTER_ERR:
+                    gtk_label_set_text(error, "Error encountered while trying to access database");
+                    break;
+                case REGISTER_DUPLICATE:
+                    gtk_label_set_text(error, "Error: A profile by that name already exists");
+                    break;
+                default:
+                    break;
+            }
+        }else gtk_label_set_text(error, "Error: Max length is 255 characters");
+    }else gtk_label_set_text(error, "Error: Do not use special characters");
+}
 
 /**
  * Initialise login window
@@ -67,8 +101,11 @@ void initLoginWindow(GtkWidget *loginWindow, gpointer data){
     ///We connect the close button to quitting
     g_signal_connect(loginWindow, "destroy", G_CALLBACK(gtk_main_quit), data);
     GtkAdjustment *scale = GTK_ADJUSTMENT(gtk_builder_get_object(uData->builder, "profilesScale"));
-    GtkLabel *error = GTK_LABEL(gtk_builder_get_object(uData->builder, "profileListError"));
+    GtkLabel *listError = GTK_LABEL(gtk_builder_get_object(uData->builder, "profileListError"));
     GtkLabel *pageLabel = GTK_LABEL(gtk_builder_get_object(uData->builder, "profilesPageLabel"));
+
+    GtkButton *registerButton = GTK_BUTTON(gtk_builder_get_object(uData->builder, "registerButton"));
+    GtkButton *loginButton = GTK_BUTTON(gtk_builder_get_object(uData->builder, "loginButton"));
 
     GtkLabel *profileLabels[] = {
             GTK_LABEL(gtk_builder_get_object(uData->builder, "profileName1")),
@@ -79,7 +116,7 @@ void initLoginWindow(GtkWidget *loginWindow, gpointer data){
     };
 
     int userCount = getUserCount(uData->db);
-    if (userCount == ACCESS_ERROR) gtk_label_set_text(error, "Error: Could not access local database");
+    if (userCount == ACCESS_ERROR) gtk_label_set_text(listError, "Error: Could not access local database");
     else{
         int nbPages = userCount / 5 + (userCount % 5 > 0 ? 1 : 0);
         gtk_adjustment_set_upper(scale, nbPages);
@@ -92,6 +129,9 @@ void initLoginWindow(GtkWidget *loginWindow, gpointer data){
         gtk_label_set_text(pageLabel, pageText);
         g_signal_connect(scale, "value-changed", G_CALLBACK(onProfilesListScroll), data);
     }
+
+    g_signal_connect(registerButton, "clicked", G_CALLBACK(onRegister), data);
+
 }
 
 /**
